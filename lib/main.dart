382a70ec0955dc021eae4a460e487d19b985e797
Tommy Tsay <tommy.tsay@hotmail.com>
2025-07-08 22:47:38 +0800

    JournalApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:timezone/data/latest.dart' as tz;
 import 'journal_app.dart';
 
 void main() {
-  tz.initializeTimeZones();
-  
- String name = 'Tommy';
- var message = 'My name is $name';
- print(message);
   runApp(const MyApp());
 }
 
